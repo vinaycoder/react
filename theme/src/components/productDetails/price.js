da164f431349ea77870591b101b04f1c4cea7509
Vinay <vinaykr.bca@gmail.com>
2018-08-13 14:23:49 +0530

    product page stock

--- theme/src/components/productDetails/price.js
+++ theme/src/components/productDetails/price.js
@@ -53,15 +53,40 @@ const Price = ({ product, variant, isAllOptionsSelected, settings }) => {
 			/>
 		);
 	} else {
+		var discountPrice=product.actual_price-product.offered_price;
+
 		return (
 			<div className="product-price-one old-price product-view-price-old invesp-price-div" style={priceStyle}>
 			<p className="price" id="old_parent_id">
-			<span className="old_price old_price-v-test"data-content="bar">Rs {product.actual_price}</span>
-			<span className="product-price-view invesp-product-price-view product-price-view-v-test-main" id="regular_price_id">Rs {product.offered_price}</span>
-		<span className="product-view-shipping product-view-shipping-v-test-main">
-		+ Free Shipping
-		</span>
-</p>
+					<span className="old_price old_price-v-test"data-content="bar">Rs {product.actual_price}</span>
+					<span className="product-price-view invesp-product-price-view product-price-view-v-test-main" id="regular_price_id">Rs {product.offered_price}</span>
+				<span className="product-view-shipping product-view-shipping-v-test-main">
+				+ Free Shipping
+				</span>
+           </p>
+          <div id="saleablediv">
+							<div className="old-price font-medium product-stock-qty product-stock-qty-mobile-ivsp" >
+									<span className="product-view-avail-in-stock product-view-avail-in-stock-v-test-main">
+
+									      {product.available_quantity>10 ? (
+														<div className="font-small variation_qty_product">
+																<strong>Low in Stock</strong>
+															</div>
+												) : (
+													<div className="inStockInProduct">
+													<span content="In Stock">In Stock</span>
+											 </div>
+												)}
+
+											<div className="clear"></div>
+								</span>
+							</div>
+			   </div>
+				 <p className="invesp-diccount-sticker-p invesp-diccount-sticker-p-v-test-main">
+					<span className="price save font-medium you-save-span you-save-span-main-add-css">You Save :
+					</span>
+					<span className="you-save-span-percent1 you-save-span-main-add-css">Rs {discountPrice} </span>
+				</p>
 				{/* <FormattedCurrency settings={settings} number={product.actual_price} /> */}
 			</div>
 		);
