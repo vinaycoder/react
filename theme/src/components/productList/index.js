ff088b739186377eac2daf3d605dee335e7ebbd7
Rahi Jain <rahijain@indiarush.com>
2018-08-18 18:50:27 +0530

    Changes for the list / grid view

--- theme/src/components/productList/index.js
+++ theme/src/components/productList/index.js
@@ -3,60 +3,84 @@ import { themeSettings, text } from '../../lib/settings';
 import Item from './item';
 import LoadMore from './loadMore';
 
-const ProductList = ({
-	products,
-	addCartItem,
-	settings,
-	loadMoreProducts,
-	hasMore,
-	productsPage,
-	loadingProducts,
-	loadingMoreProducts,
-	isCentered,
-	className = 'columns is-multiline is-mobile products',
-	columnCountOnMobile,
-	columnCountOnTablet,
-	columnCountOnDesktop,
-	columnCountOnWidescreen,
-	columnCountOnFullhd
-}) => {
-	const items = products
-		? products.map(product => (
-				<Item
-					key={product.product_id}
-					product={product}
-					addCartItem={addCartItem}
-					settings={settings}
-					columnCountOnMobile={columnCountOnMobile}
-					columnCountOnTablet={columnCountOnTablet}
-					columnCountOnDesktop={columnCountOnDesktop}
-					columnCountOnWidescreen={columnCountOnWidescreen}
-					columnCountOnFullhd={columnCountOnFullhd}
-				/>
-		  ))
-		: null;
+class ProductList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			itemView: 'grid'
+		};
+	}
 
-	return (
-		<Fragment>
-			<div
-				className={
-					className +
-					(loadingProducts ? ' loading' : '') +
-					(isCentered ? ' is-centered' : '')
-				}
-			>
-				{items}
-			</div>
-			<div className="load-more">
-				<LoadMore
-					loadMoreProducts={loadMoreProducts}
-					hasMore={hasMore}
-					productsPage={productsPage}
-					loading={loadingMoreProducts}
-				/>
-			</div>
-		</Fragment>
-	);
-};
+	onChangeToList = event => {
+		this.setState({ itemView: 'list' });
+	};
+
+	onChangeToGrid = event => {
+		this.setState({ itemView: 'grid' });
+	};
+
+	render() {
+		const {
+			products,
+			addCartItem,
+			settings,
+			loadMoreProducts,
+			hasMore,
+			productsPage,
+			loadingProducts,
+			loadingMoreProducts,
+			isCentered,
+			columnCountOnMobile,
+			columnCountOnTablet,
+			columnCountOnDesktop,
+			columnCountOnWidescreen,
+			columnCountOnFullhd
+		} = this.props;
+		const { itemView } = this.state;
+		const className = 'columns is-multiline is-mobile products';
+		const items = products
+			? products.map(product => (
+					<Item
+						key={product.product_id}
+						product={product}
+						addCartItem={addCartItem}
+						settings={settings}
+						itemView={itemView}
+						columnCountOnMobile={columnCountOnMobile}
+						columnCountOnTablet={columnCountOnTablet}
+						columnCountOnDesktop={columnCountOnDesktop}
+						columnCountOnWidescreen={columnCountOnWidescreen}
+						columnCountOnFullhd={columnCountOnFullhd}
+					/>
+			  ))
+			: null;
+
+		return (
+			<Fragment>
+				<div>
+					<div onClick={this.onChangeToGrid}>Move To Grid</div>
+					<div onClick={this.onChangeToList}>Move To List</div>
+				</div>
+				<div
+					className={
+						className +
+						(loadingProducts ? ' loading' : '') +
+						(isCentered ? ' is-centered' : '')
+					}
+				>
+					{items}
+				</div>
+				<div className="load-more">
+					<LoadMore
+						loadMoreProducts={loadMoreProducts}
+						hasMore={hasMore}
+						productsPage={productsPage}
+						loading={loadingMoreProducts}
+					/>
+				</div>
+			</Fragment>
+		);
+	}
+}
 
 export default ProductList;
--- theme/src/components/productList/item.js
+++ theme/src/components/productList/item.js
@@ -1,61 +1,103 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
 import { themeSettings, text } from '../../lib/settings';
 import ItemTags from './itemTags';
 import ItemImage from './itemImage';
 import ItemPrice from './itemPrice';
 
-const Item = ({
-	product,
-	addCartItem,
-	settings,
-	columnCountOnMobile = 2,
-	columnCountOnTablet = 3,
-	columnCountOnDesktop = 4,
-	columnCountOnWidescreen = 4,
-	columnCountOnFullhd = 4
-}) => {
-	const columnCount = 12;
+class Item extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 
-	const columnSizeOnMobile = columnCount / columnCountOnMobile;
-	const columnSizeOnTablet = columnCount / columnCountOnTablet;
-	const columnSizeOnDesktop = columnCount / columnCountOnDesktop;
-	const columnSizeOnWidescreen = columnCount / columnCountOnWidescreen;
-	const columnSizeOnFullhd = columnCount / columnCountOnFullhd;
+	render() {
+		const {
+			product,
+			addCartItem,
+			settings,
+			columnCountOnMobile = 2,
+			columnCountOnTablet = 3,
+			columnCountOnDesktop = 4,
+			columnCountOnWidescreen = 4,
+			columnCountOnFullhd = 4,
+			itemView
+		} = this.props;
 
-	const imageHeight =
-		themeSettings.list_image_max_height &&
-		themeSettings.list_image_max_height > 0
-			? themeSettings.list_image_max_height
-			: 'auto';
-	const placeholderHeight =
-		themeSettings.list_image_max_height &&
-		themeSettings.list_image_max_height > 0
-			? themeSettings.list_image_max_height
-			: 200;
+		let columnCount = 12;
+		let columnSizeOnMobile = columnCount / columnCountOnMobile;
+		let columnSizeOnTablet = columnCount / columnCountOnTablet;
+		let columnSizeOnDesktop = columnCount / columnCountOnDesktop;
+		let columnSizeOnWidescreen = columnCount / columnCountOnWidescreen;
+		let columnSizeOnFullhd = columnCount / columnCountOnFullhd;
+		if (itemView === 'list') {
+			columnSizeOnMobile = 1;
+			columnSizeOnTablet = 1;
+			columnSizeOnDesktop = 1;
+			columnSizeOnWidescreen = 1;
+			columnSizeOnFullhd = 1;
+			columnCount = 1;
+		}
 
-	return (
-		<div
-			className={`column is-${columnSizeOnMobile}-mobile is-${columnSizeOnTablet}-tablet is-${columnSizeOnDesktop}-desktop is-${columnSizeOnWidescreen}-widescreen is-${columnSizeOnFullhd}-fullhd ${
-				product.is_salable
-			}`}
-		>
-			<NavLink to={`/${product.path}/`}>
-				<figure className="image" style={{ height: imageHeight }}>
-					<ItemTags tags={product.tags} />
-					<ItemImage
-						images={product.product_images}
-						productName={product.name}
-						height={placeholderHeight}
-					/>
-				</figure>
-				<div className="content product-caption">
-					<div className="product-name">{product.name}</div>
-					<ItemPrice product={product} settings={settings} />
-				</div>
-			</NavLink>
-		</div>
-	);
-};
+		const imageHeight =
+			themeSettings.list_image_max_height &&
+			themeSettings.list_image_max_height > 0
+				? themeSettings.list_image_max_height
+				: 'auto';
+		const placeholderHeight =
+			themeSettings.list_image_max_height &&
+			themeSettings.list_image_max_height > 0
+				? themeSettings.list_image_max_height
+				: 200;
+
+		return (
+			<Fragment>
+				{itemView === 'grid' && (
+					<div
+						className={`column is-${columnSizeOnMobile}-mobile is-${columnSizeOnTablet}-tablet is-${columnSizeOnDesktop}-desktop is-${columnSizeOnWidescreen}-widescreen is-${columnSizeOnFullhd}-fullhd ${
+							product.is_salable
+						}`}
+					>
+						<NavLink to={`/${product.path}/`}>
+							<figure className="image" style={{ height: imageHeight }}>
+								{/*<ItemTags tags={product.tags} />*/}
+								<ItemImage
+									images={product.product_images}
+									productName={product.name}
+									height={placeholderHeight}
+								/>
+							</figure>
+							<div className="content product-caption">
+								<div className="product-name">{product.name}</div>
+								<ItemPrice product={product} settings={settings} />
+							</div>
+						</NavLink>
+					</div>
+				)}
+				{itemView === 'list' && (
+					<div
+						className={`row is-${columnSizeOnMobile}-mobile is-${columnSizeOnTablet}-tablet is-${columnSizeOnDesktop}-desktop is-${columnSizeOnWidescreen}-widescreen is-${columnSizeOnFullhd}-fullhd ${
+							product.is_salable
+						}`}
+					>
+						<NavLink to={`/${product.path}/`}>
+							<figure className="image" style={{ height: imageHeight }}>
+								{/*<ItemTags tags={product.tags} />*/}
+								<ItemImage
+									images={product.product_images}
+									productName={product.name}
+									height={placeholderHeight}
+								/>
+							</figure>
+							<div className="content product-caption">
+								<div className="product-name">{product.name}</div>
+								<ItemPrice product={product} settings={settings} />
+							</div>
+						</NavLink>
+					</div>
+				)}
+			</Fragment>
+		);
+	}
+}
 
 export default Item;
