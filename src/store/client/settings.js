b721cd990e0df891a3c20cb415023fb849754f64
Rahi Jain <rahijain@indiarush.com>
2018-09-01 18:45:15 +0530

    Changes for the config folder

--- src/store/client/settings.js
+++ src/store/client/settings.js
@@ -1,2 +1,8 @@
-import settings from '../../../config/store';
+// config used by store client side only
+const settings = {
+	// store UI language
+	language: 'en',
+	ajaxBaseUrl: 'http://localhost:3001/ajax'
+};
+
 export default settings;
