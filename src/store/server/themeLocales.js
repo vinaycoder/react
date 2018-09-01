ddc627d97c74eee4c87bcb3f2481f46302db1574
Rahi Jain <rahijain@indiarush.com>
2018-09-01 19:45:34 +0530

    Changes for locale centralisation

--- src/store/server/themeLocales.js
+++ src/store/server/themeLocales.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import winston from 'winston';
 
-const THEME_LOCALES_PATH = 'theme/locales/';
+const THEME_LOCALES_PATH = 'locales/';
 let text = null;
 
 export const getText = locale => {
