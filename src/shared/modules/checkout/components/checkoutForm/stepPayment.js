6a701288ece2982995dbcb84b5283e30006fe02e
Vinay Kumar <vinay.kumar@indiarush.com>
2018-09-05 14:04:16 +0530

    remove tab

--- src/shared/modules/checkout/components/checkoutForm/stepPayment.js
+++ src/shared/modules/checkout/components/checkoutForm/stepPayment.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Tabs, Tab, TabPanel, TabList } from 'react-web-tabs';
 import { themeSettings, text } from '../../../../lib/settings';
 import PaymentForm from './paymentForm';
 
@@ -31,35 +30,83 @@ const CheckoutStepPayment = props => {
 		);
 	}
 	return (
-		<div className="checkout-step">
-			<h1>
-				<span>3</span>
-				{title}
-			</h1>
+		<div className="checkout-step newPadding">
+		<div className="step-title">
+			<h2 className="not-log-in-checkout-label">3. {title}</h2>
+		</div>
+
 			<div className="checkout-button-wrap">
-				<div className="payment-form">
+				<div className="payment-form sp-methods">
 
-			<div style={{display:'flex'}}>
 
-						<div className="tab">
-							<button className="tablinks active" onClick={e => showPaymentMethod(e, 'London')} id="defaultOpen">Cod</button>
-							<button className="tablinks" onClick={e => showPaymentMethod(e, 'Paris')}>Net Banking</button>
-							<button className="tablinks" onClick={e => showPaymentMethod(e, 'Tokyo')}>Credit Card</button>
-						</div>
+				<dt className="checkout-page-radio checkout-page-hover checkout-radio-selected">
+			            <input id="p_method_cashondelivery" defaultValue="cashondelivery" type="radio" name="payment[method]" className="radio" defaultChecked autoComplete="off" />
+			        <span className="checkoutPayIcons cashondeliveryPayImageSel"></span>
+			    <label htmlFor="p_method_cashondelivery" className="payMethodCashondelivery payMethodsSel">
+			        <span className="payMethods payMethodsText">Cash On Delivery</span>
+			    </label>
+			</dt>
 
-					 <div id="London" className="tabcontent displayBlock">
-						<h3>Cod</h3>
-						</div>
+<dd className="checkout-payment-methods">
+    <fieldset className="form-list">
+<ul id="payment_form_cashondelivery">
+  <li>
+    <div className="checkout-page-summary-block-mobile-tab">
+      <h3>Order Summary</h3>
+      <table className="order-summary-table">
+			<tbody>
+			<tr>
+			<td className="CheckoutTitle">Subtotal</td>
+			<td className="CheckoutValue"><span className="price">Rs. 6,270</span></td>
+			</tr><tr><td className="CheckoutValue">Discount (IRFLAT100)</td>
+			<td className="CheckoutValue"><span className="price">-Rs. 100</span></td>
+			</tr>
+			<tr>
+			<td className="CheckoutTitle">Shipping</td>
+			<td className="CheckoutValue"><span className="price">Free</span></td>
+			</tr>
+			<tr>
+			<td className="CheckoutTitle">Cash On Delivery</td>
+			<td className="CheckoutValue"><span className="price">Rs. 50</span></td>
+			</tr>
+			<tr className="grand-total">
+			<td className="CheckoutTitle">Grand Total</td>
+			<td className="CheckoutValue"><span className="price">Rs. 6,220</span></td>
+			</tr>
+			</tbody>
+			</table>
+    </div>
+</li>
 
-						<div id="Paris" className="tabcontent displayNone">
-						<h3>Net Banking</h3>
-						</div>
+<li>
+    <div>
+        Note: Additional Rs. 50 will be charged as Cash On Delivery
+    </div>
+</li>
+  <li className="variation-checkout"> </li>
+    <li>
+      <div className="buttons-set block " id="payment-buttons-container">
+          <button type="button" title="Place Order" className="block orange-button checkoutContinueBtn btn-checkout checkout_buttons">
 
-						<div id="Tokyo" className="tabcontent displayNone">
-						<h3>Credit Card</h3>
-						</div>
+              <div>
+                <span id="paymentText">
+                    <strong>place order&nbsp;</strong>
+                </span>
+                <span id="paymentArrow" className="checkoutArrow">
+                  <img src="https://indiarush.com/skin/frontend/default/theme202/images/checkout-page-arrow-icon-13x21px.png?i=1" />
+                </span>
+              </div>
+          </button>
+      </div>
+    </li>
+</ul>
+</fieldset>
+</dd>
 
-					</div>
+
+
+
+
 
 
 
