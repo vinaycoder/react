acbafe195cb033ed548dfa16cc1eb8862f001e57
vinaykr1992 <vinaykr.bca@gmail.com>
2019-01-20 22:51:33 +0530

    add all

---
+++ assets/precache-manifest.35731d66b1b5fa9f84d416cbc1669b21.js
@@ -0,0 +1,9 @@
+self.__precacheManifest = [
+	{
+		url: '/assets/js/app-ea05aba3322bac25035f.js'
+	},
+	{
+		revision: 'ea05aba3322bac25035f',
+		url: '/assets/css/bundle-e5ad043b4482db4e2ce6.css'
+	}
+];
